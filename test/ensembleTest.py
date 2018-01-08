dc817ca0d3494c91d95f746dd3a180a1194e3cdb
Igor Pires Ferreira <ipferreira@inf.ufrgs.br>
2018-01-08 21:04:43 -0200

    Plotting

--- test/ensembleTest.py
+++ test/ensembleTest.py
@@ -9,6 +9,10 @@ from files import files
 from main import DataSet
 from ensemble import Ensemble, flatten
 from bootstrapping import fold
+import matplotlib.pyplot as plt
+#to install matplotlib
+#$ pip install matplotlib
+#$ sudo apt-get install python-tk
 
 def test(file):
     # print "\n---------------------------------------------------\n"
@@ -22,11 +26,17 @@ def test(file):
     # ntrees = [2,3,5, 10]
 
     performance = {}
+    means = []
     for ntree in ntrees:        
         ensemble = Ensemble(file, ntree)
         mean, stdDev = ensemble.crossValidation(f, ds, train)
         performance[ntree] = mean
-        print "ntree {}, mean {}, stdDev {}".format(ntree, mean, stdDev)
+        means.append(mean)
+        # print "ntree {}, mean {}, stdDev {}".format(ntree, mean, stdDev)
+    
+    #"plt.show()" interrupts execution, so remember to close it for the tests to continue
+    plt.plot(ntrees,means)
+    plt.show()
         
     bestNTree = sorted(performance, key=performance.get, reverse=True)[0]
     #TODO: evaluate best ntree. Which trained random forest should we get? 
